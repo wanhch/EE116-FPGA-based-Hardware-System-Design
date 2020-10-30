669bb6de09814558eeac3bcbe69b951de98a8827
unknown <wanhch@shanghaitech.edu.cn>
2020-10-30 23:20:25 +0800

    hw3

--- lab4/Lab4_handin/src/pipe.vhd
+++ lab4/Lab4_handin/src/pipe.vhd
@@ -5,13 +5,13 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity pipe is
     Generic(
-            DATA_WIDTH : POSITIVE := 8;
-            LENGTH : POSITIVE := 10);  
-    Port ( 
-            clk : in STD_LOGIC;
-            rst : in STD_LOGIC;
-            D : in STD_LOGIC_VECTOR (DATA_WIDTH-1 downto 0);
-            Q : out STD_LOGIC_VECTOR (DATA_WIDTH-1 downto 0) := (others => '0'));
+        DATA_WIDTH : POSITIVE := 8;
+        LENGTH : POSITIVE := 10);  
+    Port( 
+        clk : in STD_LOGIC;
+        rst : in STD_LOGIC;
+        D : in STD_LOGIC_VECTOR (DATA_WIDTH-1 downto 0);
+        Q : out STD_LOGIC_VECTOR (DATA_WIDTH-1 downto 0) := (others => '0'));
 end pipe;
 
 architecture Behavioral of pipe is
