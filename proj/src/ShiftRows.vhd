8ca5d791ef32913a129ea6bb409a8517fe2b6c37
unknown <wanhch@shanghaitech.edu.cn>
2020-11-28 00:08:51 +0800

    finish ShiftRows

--- proj/src/ShiftRows.vhd
+++ proj/src/ShiftRows.vhd
@@ -21,9 +21,21 @@ begin
             reg <= (others => '0');
         elsif (rising_edge(clk)) then
             if (mode = '0') then
-
+                reg(31 downto 0) <= in(31 downto 0 );
+                reg(55 downto 32) <= in(63 downto 40);
+                reg(63 downto 56) <= in(39 downto 32);
+                reg(79 downto 64) <= in(95 downto 80);
+                reg(95 downto 80) <= in(79 downto 64);
+                reg(103 downto 96) <= in(127 downto 120);
+                reg(127 downto 104) <= in(119 downto 96);
             elsif (mode = '1') then
-
+                reg(31 downto 0 ) <= in(31 downto 0);
+                reg(63 downto 40) <= in(55 downto 32);
+                reg(39 downto 32) <= in(63 downto 56);
+                reg(95 downto 80) <= in(79 downto 64);
+                reg(79 downto 64) <= in(95 downto 80);
+                reg(127 downto 120) <= in(103 downto 96);
+                reg(119 downto 96) <= in(127 downto 104);
             else 
                 reg <= (others => '0');
             end if;
